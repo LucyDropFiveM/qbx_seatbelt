4bc78638e033d3c66387994087acd1d3dce9b299
LucyDrop <78080230+LucyDropFiveM@users.noreply.github.com>
2024-09-01 10:23:23 +0200

    Create sv.lua
    Swedish lang for the qbx_seatbelt for our Swedish users

---
+++ locales/sv.lua
@@ -0,0 +1,14 @@
+{
+    "progress" : {
+        "attachHarness": "Spänner åt säkerhetssele",
+        "removeHarness": "Tar loss säkerhetssele"
+    },
+    "notify": {
+        "notInCar": "Du är inte i en bil"
+    },
+    "error": {
+        "seatbelton": "Ta av ditt säkerhetsbälte först",
+        "harnesson": "Ta av din säkerhetssele först"
+    },
+    "toggleCommand": "Sätt på bälte"
+}
